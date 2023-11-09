c1d0e828ba0616aaa7aa2a6f72470db8fdf42293
NachoBosch <nachobosch5@gmail.com>
2023-11-09 16:56:29 -0300

    gráfico de evolución del algoritmo en 100 corridas

--- jMetalPy/SumSubset.py
+++ jMetalPy/SumSubset.py
@@ -5,11 +5,19 @@ from jmetal.operator.crossover import PMXCrossover
 from jmetal.operator.selection import BestSolutionSelection
 from jmetal.util.termination_criterion import StoppingByEvaluations
 from jmetal.util.observer import ProgressBarObserver
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 pbar = ProgressBarObserver(max=100)
 
 pesos = [10, 20, 15, 25, 5]
 objetivo = 30
+
+'''
+Problema de optimización combinatoria que busca un subconjunto de elementos, dado un conjunto, de tal manera que la suma de los elementos en el subconjunto sea igual a un valor objetivo específico.
+Tiene dos atributos importantes que son: C (large integer) valor objetivo y W (set of non-negative integers) array que representa el conjunto  de elementos.
+'''
 problema = SubsetSum(C = objetivo,
                      W = pesos)
 
@@ -18,7 +26,7 @@ algoritmo_ga = GeneticAlgorithm(problema,
                             population_size=100,
                             offspring_population_size=20,
                             mutation=IntegerPolynomialMutation(0.1, 20), 
-                            crossover=PMXCrossover(0.6), # probabilidad de cruce del 0.9. de que para cada par de soluciones en la población se realice el cruce y así generar una nueva solución en cada generación
+                            crossover=PMXCrossover(0.6),
                             selection=BestSolutionSelection(),
                             termination_criterion=StoppingByEvaluations(max_evaluations=100)
                             )
@@ -32,3 +40,22 @@ algoritmo_ga.run()
 soluciones_ga = algoritmo_ga.get_result()
 print(f"Soluciones Genetic Algorithm: {soluciones_ga.variables}\n")
 print(f"Mejor solución: {soluciones_ga.objectives}\n")
+
+
+## Optmización con gráfica
+evolucion = []
+mejor_solucion = 0 
+for generacion in range(100):
+    algoritmo_ga.run()
+    soluciones = algoritmo_ga.get_result()
+    mejor_solucion_generacion = soluciones.objectives[0]
+    evolucion.append(mejor_solucion_generacion)
+    if mejor_solucion_generacion <= min(evolucion):
+        mejor_solucion = mejor_solucion_generacion
+
+print(f"Mejor solución: {mejor_solucion}")
+plt.plot(evolucion,c='purple')
+plt.xlabel("Generación")
+plt.ylabel("Suma")
+plt.title("Suma de enteros")
+plt.show()
