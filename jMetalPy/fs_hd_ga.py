b77eacbd4843ee34d0fa776293deaa404c86b6f4
NachoBosch <nachobosch5@gmail.com>
2024-05-07 22:28:42 -0300

    Resultados experimentos funcion agregativa jmetalpy

--- jMetalPy/fs_hd_ga.py
+++ jMetalPy/fs_hd_ga.py
@@ -24,9 +24,10 @@ from sklearn.feature_selection import SelectKBest, f_classif
 
 #PROBLEM
 class FeatureSelectionProblem():
-  def __init__(self,X,y):
+  def __init__(self,X,y,alfa):
     self.X = X
     self.y = y
+    self.alfa = alfa
     self.number_of_variables = X.shape[1]
     self.number_of_objectives = 1
     self.number_of_constraints = 0
@@ -43,10 +44,9 @@ class FeatureSelectionProblem():
     acc = accuracy_score(ytest, y_pred)
 
     num_variables = len(selected_features)
-    alfa = 0.7
-    beta = 1 - alfa
+    beta = 1 - self.alfa
     fitness = 1.0 - (num_variables/self.X.shape[1]) # Primera parte de la función agregativa
-    fitness = (alfa * fitness) + (beta * acc)
+    fitness = (self.alfa * fitness) + (beta * acc)
     solution.objectives[0] = -fitness
     solution.constraints = []
 
@@ -76,26 +76,38 @@ y = encoder.fit_transform(df_hd.Grade.to_numpy())
 clases = list(df_hd.columns[:-2])
 
 #PRE-FILTER
-kbest = SelectKBest(score_func=f_classif, k=100)
-X_select = kbest.fit_transform(X, y)
-print("Columnas seleccionadas KBest:", len(kbest.get_support(indices=True)))
-selected_features = [clases[i] for i in kbest.get_support(indices=True)]
+# kbest = SelectKBest(score_func=f_classif, k=100)
+# X = kbest.fit_transform(X, y)
+# print("Columnas seleccionadas KBest:", len(kbest.get_support(indices=True)))
+# selected_features = [clases[i] for i in kbest.get_support(indices=True)]
 # print(f"Features seleccionadas KBest: {selected_features}")
 
 #PROBLEM
-problem = FeatureSelectionProblem(X_select,y)
+alfa = 0.6
+problem = FeatureSelectionProblem(X,y,alfa)
+
+#PARAMETERS
 pobl = 150
 off_pobl = int(pobl*0.75)
+evals = 7500
+mut_p = 0.01
+cross_p = 0.9
+
+#OPERATORS
+mut = BitFlipMutation(mut_p)
+cross = SPXCrossover(cross_p)
+selection = BestSolutionSelection()
+criterion = StoppingByEvaluations(max_evaluations=evals)
 
 # # ALGORITHM
 algorithm = GeneticAlgorithm(
     problem=problem,
     population_size=pobl,
     offspring_population_size=off_pobl,
-    mutation=BitFlipMutation(0.01),
-    crossover=SPXCrossover(0.9),
-    selection=BestSolutionSelection(),
-    termination_criterion=StoppingByEvaluations(max_evaluations=1200)
+    mutation=mut,
+    crossover=cross,
+    selection=selection,
+    termination_criterion=criterion
 )
 
 algorithm.run()
@@ -108,7 +120,7 @@ variables = soluciones_ls.variables
 var_squeezed = np.squeeze(variables)
 genes_selected = [gen for gen,var in zip(clases,var_squeezed) if var==1]
 
-with open('Resultados_FS_2.txt','w') as f:
+with open('Resultados_func_agregativa/Experimento3/Resultados_FS_3.txt','w') as f:
   f.write(f"Name: {algorithm.get_name()}\n")
   f.write(f"Solucion objectives: {objectives}\n")
   f.write(f"Solucion variables: {variables}\n")
@@ -116,3 +128,10 @@ with open('Resultados_FS_2.txt','w') as f:
   f.write(f"Solucion variables amount: {len(variables)}\n")
   f.write(f"Selected genes: {genes_selected}\n")
   f.write(f"Selected genes amount: {len(genes_selected)}\n")
+  f.write(f"Agregative function parameters alfa: {alfa}, beta: {1-alfa}\n")
+  f.write(f"Stopping criterion: {criterion} | parameter: {evals}\n")
+  f.write(f"Crossover: {cross} | parameter: {cross_p}\n")
+  f.write(f"Mutation: {mut} | parameter: {mut_p}\n")
+  f.write(f"Poblation size: {pobl} | Offspring size: {off_pobl}\n")
+  f.write(f"Clases: {encoder.classes_}\n")
+  f.write(f"Data input shape: {X.shape}\n")
