c66205d5fe5c071ea70a0abc53622f52105b7773
NachoBosch <nachobosch5@gmail.com>
2025-04-06 10:53:00 -0300

    alfa 0.2 en SGA

--- jMetalPy/experimentos/FS_SGA.py
+++ jMetalPy/experimentos/FS_SGA.py
@@ -51,7 +51,7 @@ def configure_experiment(problems: dict,n_run: int):
     return jobs
 
 data = load.huntington()
-alfa = 0.1
+alfa = 0.2
 models_names, models = load.models()
 # tupla = load.models()
 # model_name = tupla[0][-1]
